7509f139416bf2ba613dfbdc55fbae299e48e3d4
lilulab <lilu12@andrew.cmu.edu>
2015-08-06 18:53:00 -0700

    Modified Version of the CwruStim Library  - Add most of the UECU command sets.  - Add Mssage type and length #define  - Change the checkSum func. to use uint datatype

--- CwruStim.cpp
+++ CwruStim.cpp
@@ -7,12 +7,12 @@
 #include "Arduino.h"
 #include "CwruStim.h"
 
-int Stim::_channel_id = 0;
+int Stim::_uart_channel_id = 0;
 int Stim::_mode = 0;
 int Stim::_setting = 0;
 int Stim::_stim_error = 0;
 int Stim::_command = 0;
-
+ 
 int Stim::_dir = 1;
 int Stim::_min = 0x00;
 int Stim::_max = 0xFA;
@@ -22,6 +22,7 @@ uint8_t Stim::_del_sched[6] = {0x04, 0x80, 0x12, 0x01, 0x01, 0x67};
 uint8_t Stim::_chan_set1[12] = {0x04, 0x80, 0x47, 0x07, 0x00, 0x64, 0xFA, 0x00, 0x64, 0x11, 0x01, 0x57};
 uint8_t Stim::_chan_set2[12] = {0x04, 0x80, 0x47, 0x07, 0x00, 0x64, 0xFA, 0x00, 0x64, 0x11, 0x23, 0x35};
 uint8_t Stim::_chan_set3[12] = {0x04, 0x80, 0x47, 0x07, 0x00, 0x64, 0xFA, 0x00, 0x64, 0x11, 0x45, 0x13};
+
 uint8_t Stim::_crt_sched[8] = {0x04, 0x80, 0x10, 0x03, 0xAA, 0x00, 0x1D, 0xA0};
 uint8_t Stim::_crt_evnt1[14] = {0x04, 0x80, 0x15, 0x09, 0x01, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x59}; 
 uint8_t Stim::_crt_evnt2[14] = {0x04, 0x80, 0x15, 0x09, 0x01, 0x00, 0x05, 0x00, 0x03, 0x01, 0x00, 0x00, 0x00, 0x53}; 
@@ -31,12 +32,16 @@ uint8_t Stim::_sync_msg1[6] = {0x04, 0x80, 0x1B, 0x01, 0xAA, 0xB4};
 uint8_t Stim::_chngevnt1[9] = {0x04, 0x80, 0x19, 0x04, 0x01, 0x00, 0x3C, 0x00, 0x00};
 
 // Stim constructor and UART selector
-Stim::Stim(int channel_id) {
+
+
+Stim::Stim(int uart_channel_id) {
 	// Initialize the NTREK ECB to connect the Stim Board
 	// Check which UART channel is needed
+	// Save to private var
+	_uart_channel_id = uart_channel_id;
 	
 	//Serial.println("Enter Stim constructor");
-	switch (_channel_id) {
+	switch (_uart_channel_id) {
 
 		case STIM_CHANNEL_UART0:
 			//Use UART1(TX1&RX1) port to connect Stim Board
@@ -70,8 +75,6 @@ Stim::Stim(int channel_id) {
 		//Serial.println("exit Stim constructor");
 	}
 
-	// Save to private var
-	_channel_id = channel_id;
 }
 
 // Initialize Stim Board via UART
@@ -86,7 +89,7 @@ int Stim::init(int mode) {
 	return 1;
 }
 
-// Configurate Stim Board via UART
+// Configure Stim Board via UART
 int Stim::config(int setting) {
 
 	//Serial.println("Enter Stim Config");
@@ -130,6 +133,8 @@ int Stim::serial_write_array(uint8_t buf[], int length) {
 	//Serial.write (buf,4);
 	//Serial.println(buf);
 
+	// ToDo: switch different UART channel.
+	
 	for(int i = 0; i<length; i++){
 		Serial.write(buf[i]);
 		//Serial.print(buf[i],HEX);
@@ -147,11 +152,75 @@ int Stim::serial_write_array(uint8_t buf[], int length) {
 	return Serial;
 }
 
-int Stim::checksum(int vals[], int length){
-  int csum = 0;
+uint8_t Stim::checksum(uint8_t vals[], int length){
+  uint8_t csum = 0;
   for(int i=0; i<length-1; i++) {
     csum += vals[i];
   }
   csum = ((0x00FF & csum) + (csum >> 8))^0xFF;
   return csum;
 } 
+
+// uint8_t Stim::_chan_seti[12] = {0x04, 0x80, 0x47, 0x07, 0x00, 0x64, 0xFA, 0x00, 0x64, 0x11, 0x01, 0x57};
+// UECU command sets
+//UECU Halt
+int Stim::cmd_halt_rset(int halt_flag) {
+	// calculate message size
+	int size = HALT_MSG_LEN + UECU_MSG_EXTRAL_LEN;
+	// build message content
+	uint8_t msg[HALT_MSG_LEN + UECU_MSG_EXTRAL_LEN] = 
+	{ 	MSG_DES_ADDR,
+		MSG_SRC_ADDR,
+		HALT_MSG,
+		HALT_MSG_LEN,
+		halt_flag,
+		0x00
+	};
+
+	// Insert checksum byte
+	msg[size-1] = this->checksum(msg,size);
+
+	// Send message
+	return this->serial_write_array (msg,sizeof(msg)/sizeof(uint8_t));
+
+}
+
+// UECU Delete Schedule
+int Stim::cmd_del_sched(int sched_id) {
+
+}
+
+// UECU Create Schedule
+int Stim::cmd_crt_sched(int sync_signal, int duration) {
+
+}
+
+// UECU Channel Setup 
+int Stim::cmd_chan_set(	int port_chn_id, 
+                        int amp_limit,
+                        int ip_delay, 
+                        int asp_ratio, 
+                        int anode_cathode) {
+
+}
+
+// UECU Create Event
+int Stim::cmd_crt_evnt( int sched_id, 
+                        int delay, 
+                      	int priority, 
+                      	int event_type, 
+                      	int port_chn_id) {
+
+}
+// UECU Change Event Parameter Command
+int Stim::cmd_set_evnt( int event_id,
+                        int pulse_width,
+                        int amplitude) {
+
+}
+// UECU Change Schedule Message
+int Stim::cmd_set_sched( int sched_id,
+                         int sync_signal,
+                         int duration) {
+
+}
