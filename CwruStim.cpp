982b5987c6902e897932cc9be00f668e0eafc06c
lilulab <lilu12@andrew.cmu.edu>
2015-08-07 00:51:41 -0400

    Finish up all the UECU command  - Add function for Stim::cmd_*  - Change all of the Stim::cmd_* input type  - Notice that the duration, delay, ip_delay take 2bytes.

--- CwruStim.cpp
+++ CwruStim.cpp
@@ -86,31 +86,129 @@ int Stim::init(int mode) {
 	_mode = mode;
 	//Serial.println("Exit Stim Initialization");
 
+	delay(50); //wait UECU power up
+
+    // Reset Halt Message to run UECU
+	this->cmd_halt_rset(UECU_RESET);
+
+	// Delete Schedule
+	this->cmd_del_sched(1); // Delete schedule 1
+
 	return 1;
 }
 
 // Configure Stim Board via UART
 int Stim::config(int setting) {
 
-	//Serial.println("Enter Stim Config");
+	/* // Magic Numbers Method
+	// //Serial.println("Enter Stim Config");
 
-	delay(50);
-	this->serial_write_array (_halt_rset,sizeof(_halt_rset)/sizeof(uint8_t));
-	//this->serial_write_array (del_sched);
+	// delay(50);
+	// this->serial_write_array (_halt_rset,sizeof(_halt_rset)/sizeof(uint8_t));
+	// //this->serial_write_array (del_sched);
 	
-	this->serial_write_array (_chan_set1,sizeof(_chan_set1)/sizeof(uint8_t));
-	this->serial_write_array (_chan_set2,sizeof(_chan_set2)/sizeof(uint8_t));
-	this->serial_write_array (_chan_set3,sizeof(_chan_set3)/sizeof(uint8_t));
-	this->serial_write_array (_crt_sched,sizeof(_crt_sched)/sizeof(uint8_t));
-	this->serial_write_array (_crt_evnt1,sizeof(_crt_evnt1)/sizeof(uint8_t));
-	this->serial_write_array (_crt_evnt2,sizeof(_crt_evnt2)/sizeof(uint8_t));
-	this->serial_write_array (_crt_evnt3,sizeof(_crt_evnt3)/sizeof(uint8_t));
-	this->serial_write_array (_sync_msg1,sizeof(_sync_msg1)/sizeof(uint8_t));
+	// this->serial_write_array (_chan_set1,sizeof(_chan_set1)/sizeof(uint8_t));
+	// this->serial_write_array (_chan_set2,sizeof(_chan_set2)/sizeof(uint8_t));
+	// this->serial_write_array (_chan_set3,sizeof(_chan_set3)/sizeof(uint8_t));
+	// this->serial_write_array (_crt_sched,sizeof(_crt_sched)/sizeof(uint8_t));
+	// this->serial_write_array (_crt_evnt1,sizeof(_crt_evnt1)/sizeof(uint8_t));
+	// this->serial_write_array (_crt_evnt2,sizeof(_crt_evnt2)/sizeof(uint8_t));
+	// this->serial_write_array (_crt_evnt3,sizeof(_crt_evnt3)/sizeof(uint8_t));
+	// this->serial_write_array (_sync_msg1,sizeof(_sync_msg1)/sizeof(uint8_t));
 
-	_chngevnt1[5] = _min + 1;
+	// _chngevnt1[5] = _min + 1;
 
-	_setting = setting;
-	//Serial.println("Exit Stim Config");
+	// _setting = setting;
+	// //Serial.println("Exit Stim Config");
+	*/ // Magic Numbers Method
+
+	// Channels setup
+	//cmd_chan_set(port_chn_id, amp_limit, pw_limit, ip_delay, asp_ratio, anode_cathode);
+	
+	// Bipolar 01
+	this->cmd_chan_set(	0,		// port_chn_id =  0
+						100,	// amp_limit = 100mA
+						0xFA,	// pw_limit = 250usec
+						100,	// ip_delay = 100usec
+						0x11, 	// asp_ratio = 1:1
+						0x01);	//anode = 0, cathode = 1, for bipolar mode);
+
+	// Bipolar 23
+	this->cmd_chan_set(	1,		// port_chn_id =  0
+						100,	// amp_limit = 100mA
+						0xFA,	// pw_limit = 250usec
+						100,	// ip_delay = 100usec
+						0x11, 	// asp_ratio = 1:1
+						0x23);	//anode = 2, cathode = 3, for bipolar mode);
+
+	// Bipolar 45
+	this->cmd_chan_set(	2,		// port_chn_id =  0
+						100,	// amp_limit = 100mA
+						0xFA,	// pw_limit = 250usec
+						100,	// ip_delay = 100usec
+						0x11, 	// asp_ratio = 1:1
+						0x45);	//anode = 0, cathode = 1, for bipolar mode);
+
+	// Bipolar 67
+	this->cmd_chan_set(	3,		// port_chn_id =  0
+						100,	// amp_limit = 100mA
+						0xFA,	// pw_limit = 250usec
+						100,	// ip_delay = 100usec
+						0x11, 	// asp_ratio = 1:1
+						0x67);	//anode = 0, cathode = 1, for bipolar mode);
+
+
+
+	// Create Schedule
+	// this->cmd_crt_sched(sync_signal, duration);
+	this->cmd_crt_sched(0xAA, 29);	// Sync signal = 0xAA, duration 29msec.
+
+	// Create Events
+	// this->cmd_crt_evnt(sched_id, delay, priority, event_type, port_chn_id);
+
+	// Create Event 1 for port_chn_id 0 in sched_id 1 
+	this->cmd_crt_evnt( 1,	// sched_id = 1
+						0,	// delay = 0msec
+						0,	// priority = 0
+						3,	// event_type = 3, for for Stimulus Event
+						0,	// port_chn_id = 0;
+						0,	// pulse_width set to 0,
+                      	0,	// amplitude set to 0,
+                      	0);	// zone not implemented);
+
+	// Create Event 2 for port_chn_id 0 in sched_id 1 
+	this->cmd_crt_evnt( 1,	// sched_id = 1
+						0,	// delay = 0msec
+						0,	// priority = 0
+						3,	// event_type = 3, for for Stimulus Event
+						1,	// port_chn_id = 1;
+						0,	// pulse_width set to 0,
+                      	0,	// amplitude set to 0,
+                      	0);	// zone not implemented);
+
+	// Create Event 3 for port_chn_id 0 in sched_id 1 
+	this->cmd_crt_evnt( 1,	// sched_id = 1
+						0,	// delay = 0msec
+						0,	// priority = 0
+						3,	// event_type = 3, for for Stimulus Event
+						2,	// port_chn_id = 2;
+						0,	// pulse_width set to 0,
+                      	0,	// amplitude set to 0,
+                      	0);	// zone not implemented);
+
+	// Create Event 4 for port_chn_id 0 in sched_id 1 
+	this->cmd_crt_evnt( 1,	// sched_id = 1
+						0,	// delay = 0msec
+						0,	// priority = 0
+						3,	// event_type = 3, for for Stimulus Event
+						3,	// port_chn_id = 3;
+						0,	// pulse_width set to 0,
+                      	0,	// amplitude set to 0,
+                      	0);	// zone not implemented);
+
+	// Send Sync to start
+	this->cmd_sync_msg(0xAA); // Sent Sync_message 0xAA to start schedule.
+
 	return 1;
 }
 
@@ -164,7 +262,7 @@ uint8_t Stim::checksum(uint8_t vals[], int length){
 // uint8_t Stim::_chan_seti[12] = {0x04, 0x80, 0x47, 0x07, 0x00, 0x64, 0xFA, 0x00, 0x64, 0x11, 0x01, 0x57};
 // UECU command sets
 //UECU Halt
-int Stim::cmd_halt_rset(int halt_flag) {
+int Stim::cmd_halt_rset(uint8_t halt_flag) {
 	// calculate message size
 	int size = HALT_MSG_LEN + UECU_MSG_EXTRAL_LEN;
 	// build message content
@@ -186,41 +284,184 @@ int Stim::cmd_halt_rset(int halt_flag) {
 }
 
 // UECU Delete Schedule
-int Stim::cmd_del_sched(int sched_id) {
+int Stim::cmd_del_sched(uint8_t sched_id) {
+	// calculate message size
+	int size = DELETE_SCHEDULE_MSG_LEN + UECU_MSG_EXTRAL_LEN;
+	// build message content
+	uint8_t msg[DELETE_SCHEDULE_MSG_LEN + UECU_MSG_EXTRAL_LEN] = 
+	{ 	MSG_DES_ADDR,
+		MSG_SRC_ADDR,
+		DELETE_SCHEDULE_MSG,
+		DELETE_SCHEDULE_MSG_LEN,
+		sched_id,
+		0x00
+	};
 
+	// Insert checksum byte
+	msg[size-1] = this->checksum(msg,size);
+
+	// Send message
+	return this->serial_write_array (msg,sizeof(msg)/sizeof(uint8_t));
 }
 
 // UECU Create Schedule
-int Stim::cmd_crt_sched(int sync_signal, int duration) {
+int Stim::cmd_crt_sched(uint8_t sync_signal, uint16_t duration) {
+	// calculate message size
+	int size = CREATE_SCHEDULE_MSG_LEN + UECU_MSG_EXTRAL_LEN;
+	// build message content
+	uint8_t msg[CREATE_SCHEDULE_MSG_LEN + UECU_MSG_EXTRAL_LEN] = 
+	{ 	MSG_DES_ADDR,
+		MSG_SRC_ADDR,
+		CREATE_SCHEDULE_MSG,
+		CREATE_SCHEDULE_MSG_LEN,
+		sync_signal,
+		(uint8_t)((duration >> 8) & 0x00FF),
+		(uint8_t)(duration & 0x00FF),
+		0x00
+	};
 
+	// Insert checksum byte
+	msg[size-1] = this->checksum(msg,size);
+
+	// Send message
+	return this->serial_write_array (msg,sizeof(msg)/sizeof(uint8_t));
 }
 
 // UECU Channel Setup 
-int Stim::cmd_chan_set(	int port_chn_id, 
-                        int amp_limit,
-                        int ip_delay, 
-                        int asp_ratio, 
-                        int anode_cathode) {
+int Stim::cmd_chan_set(	uint8_t port_chn_id, 
+                        uint8_t amp_limit,
+                        uint8_t pw_limit,
+                        uint16_t ip_delay, 
+                        uint8_t asp_ratio, 
+                        uint8_t anode_cathode) {
+	// calculate message size
+	int size = CHANNEL_SETUP_MSG_LEN + UECU_MSG_EXTRAL_LEN;
+	// build message content
+	uint8_t msg[CHANNEL_SETUP_MSG_LEN + UECU_MSG_EXTRAL_LEN] = 
+	{ 	MSG_DES_ADDR,
+		MSG_SRC_ADDR,
+		CHANNEL_SETUP_MSG,
+		CHANNEL_SETUP_MSG_LEN,
+		port_chn_id,
+		amp_limit,
+		pw_limit,
+		(uint8_t)((ip_delay >> 8) & 0x00FF),
+		(uint8_t)(ip_delay & 0x00FF),
+		asp_ratio,
+		anode_cathode,
+		0x00
+	};
 
+	// Insert checksum byte
+	msg[size-1] = this->checksum(msg,size);
+
+	// Send message
+	return this->serial_write_array (msg,sizeof(msg)/sizeof(uint8_t));
 }
 
 // UECU Create Event
-int Stim::cmd_crt_evnt( int sched_id, 
-                        int delay, 
-                      	int priority, 
-                      	int event_type, 
-                      	int port_chn_id) {
+int Stim::cmd_crt_evnt( uint8_t sched_id, 
+                        uint16_t delay, 
+                      	uint8_t priority, 
+                      	uint8_t event_type, 
+                      	uint8_t port_chn_id,
+                      	uint8_t pulse_width,
+                      	uint8_t amplitude,
+                      	uint8_t zone) {
+	// calculate message size
+	int size = CREATE_EVENT_MSG_LEN + UECU_MSG_EXTRAL_LEN;
+	// build message content
+	uint8_t msg[CREATE_EVENT_MSG_LEN + UECU_MSG_EXTRAL_LEN] = 
+	{ 	MSG_DES_ADDR,
+		MSG_SRC_ADDR,
+		CREATE_EVENT_MSG,
+		CREATE_EVENT_MSG_LEN,
+		sched_id,
+		(uint8_t)((delay >> 8) & 0x00FF),
+		(uint8_t)(delay & 0x00FF),
+		priority,
+		event_type,
+		port_chn_id,
+		pulse_width, //Param[1]
+		amplitude, //Param[2]
+		zone, //Param[3] not implemented
+		0x00
+	};
 
+	// Insert checksum byte
+	msg[size-1] = this->checksum(msg,size);
+
+	// Send message
+	return this->serial_write_array (msg,sizeof(msg)/sizeof(uint8_t));
 }
 // UECU Change Event Parameter Command
-int Stim::cmd_set_evnt( int event_id,
-                        int pulse_width,
-                        int amplitude) {
+int Stim::cmd_set_evnt( uint8_t event_id,
+                        uint8_t pulse_width,
+                        uint8_t amplitude) {
+	// calculate message size
+	int size = CHANGE_EVENT_PARAMS_MSG_LEN + UECU_MSG_EXTRAL_LEN;
+	// build message content
+	uint8_t msg[CHANGE_EVENT_PARAMS_MSG_LEN + UECU_MSG_EXTRAL_LEN] = 
+	{ 	MSG_DES_ADDR,
+		MSG_SRC_ADDR,
+		CHANGE_EVENT_PARAMS_MSG,
+		CHANGE_EVENT_PARAMS_MSG_LEN,
+		event_id,
+		pulse_width,
+		amplitude,
+		0x00, //Param?
+		0x00
+	};
 
+	// Insert checksum byte
+	msg[size-1] = this->checksum(msg,size);
+
+	// Send message
+	return this->serial_write_array (msg,sizeof(msg)/sizeof(uint8_t));
 }
 // UECU Change Schedule Message
-int Stim::cmd_set_sched( int sched_id,
-                         int sync_signal,
-                         int duration) {
+int Stim::cmd_set_sched( uint8_t sched_id,
+                         uint8_t sync_signal,
+                         uint16_t duration) {
+	// calculate message size
+	int size = CHANGE_EVENT_SCHED_MSG_LEN + UECU_MSG_EXTRAL_LEN;
+	// build message content
+	uint8_t msg[CHANGE_EVENT_SCHED_MSG_LEN + UECU_MSG_EXTRAL_LEN] = 
+	{ 	MSG_DES_ADDR,
+		MSG_SRC_ADDR,
+		CHANGE_EVENT_SCHED_MSG,
+		CHANGE_EVENT_SCHED_MSG_LEN,
+		sched_id,
+		sync_signal,
+		(uint8_t)((duration >> 8) & 0x00FF),
+		(uint8_t)(duration & 0x00FF),
+		0x00
+	};
 
+	// Insert checksum byte
+	msg[size-1] = this->checksum(msg,size);
+
+	// Send message
+	return this->serial_write_array (msg,sizeof(msg)/sizeof(uint8_t));
+}
+
+// UECU Sync Message
+int Stim::cmd_sync_msg( uint8_t sync_signal) {
+	// calculate message size
+	int size = SYNC_MSG_LEN + UECU_MSG_EXTRAL_LEN;
+	// build message content
+	uint8_t msg[SYNC_MSG_LEN + UECU_MSG_EXTRAL_LEN] = 
+	{ 	MSG_DES_ADDR,
+		MSG_SRC_ADDR,
+		SYNC_MSG,
+		SYNC_MSG_LEN,
+		sync_signal,
+		0x00
+	};
+
+	// Insert checksum byte
+	msg[size-1] = this->checksum(msg,size);
+
+	// Send message
+	return this->serial_write_array (msg,sizeof(msg)/sizeof(uint8_t));
 }
